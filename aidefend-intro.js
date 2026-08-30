22be0a1017284969ed467cde80071a05f01220b6
edward-aidefendlabs <edward@aidefendlabs.com>
2026-08-30 09:13:19 -0700

    Minor Update: Correction on date accuracy

--- aidefend-intro.js
+++ aidefend-intro.js
@@ -1,4 +1,4 @@
-export const aidefendVersion = "1.20260830";
+export const aidefendVersion = "1.20260831";
 
 export const aidefendIntroduction = {
     "mainTitle": "About AIDEFEND™ - An AI Defense Framework",
